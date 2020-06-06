9eec33da888a5d6236fa082db1560c72784cd71f
Ryan Wiggins <ryanwiggins52@gmail.com>
2020-06-06 12:37:47 -0400

    passed a label to display, will store its checked state and will be rendered with a unique id

--- moz-todo-vue/moz-todo-vue/src/App.vue
+++ moz-todo-vue/moz-todo-vue/src/App.vue
@@ -3,7 +3,7 @@
     <h1>To-Do List</h1>
     <ul>
       <li>
-        <to-do-item></to-do-item>
+        <to-do-item label="My ToDo Item" :done="true"></to-do-item>
       </li>
     </ul>
   </div>
--- moz-todo-vue/moz-todo-vue/src/components/ToDoItem.vue
+++ moz-todo-vue/moz-todo-vue/src/components/ToDoItem.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
-    <input type="checkbox" id="todo-item" checked="false" />
-    <label for="todo-item">My Todo Item</label>
+    <input type="checkbox" id="todo-item" :checked="isDone" />
+    <label :for="id">{{label}}</label>
   </div>
 </template>
 <script>
-export default {};
+import uniqueId from "lodash.uniqueid";
+export default {
+  props: {
+    label: { required: true, type: String },
+    done: { default: false, type: Boolean }
+  },
+  data() {
+    return {
+      isDone: this.done,
+      id: uniqueId("todo-")
+    };
+  }
+};
 </script>
