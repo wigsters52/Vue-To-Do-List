3795b9f3c59be3f0ee28e061c36128d6a4a8d8a8
Ryan Wiggins <ryanwiggins52@gmail.com>
2020-06-07 19:33:56 -0400

    added some styling

--- moz-todo-vue/moz-todo-vue/src/components/ToDoForm.vue
+++ moz-todo-vue/moz-todo-vue/src/components/ToDoForm.vue
@@ -1,14 +1,21 @@
 <template>
   <form @submit.prevent="onSubmit">
-    <label for="new-todo-input">What needs to be done?</label>
+    <h2 class="label-wrapper">
+      <label for="new-todo-input" class="label__lg checkbox-label">
+        What needs to be done?
+      </label>
+    </h2>
     <input
       type="text"
       id="new-todo-input"
       name="new-todo"
       autocomplete="off"
       v-model.lazy.trim="label"
+      class="input__lg checkbox"
     />
-    <button type="submit">Add</button>
+    <button type="submit" class="btn btn__primary btn__lg">
+      Add
+    </button>
   </form>
 </template>
 
